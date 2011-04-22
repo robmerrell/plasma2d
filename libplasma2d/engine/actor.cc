73511d74f0e162b1292be0a008a1ecdbb94b8a7b
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2011-04-21 22:30:11 -0600

    Optimized Rotating and Scaling matrices

--- libplasma2d/engine/actor.cc
+++ libplasma2d/engine/actor.cc
@@ -24,7 +24,7 @@ p2d::Actor::~Actor() {
 
 
 void p2d::Actor::draw() {
-//    // TODO: this shouldn't be rebuilt every frame...
+    // TODO: this should be done in the constructor
     static const GLfloat squareVertices[] = {
         0.0f, 0.0f, 0.0f,
         width, 0.0f, 0.0f,
@@ -33,16 +33,21 @@ void p2d::Actor::draw() {
     };
     
     // change the anchor point
+    // TODO: this should be done in setAnchor, not here
     float anchor_x = width * -anchor.x;
     float anchor_y = height * -anchor.y;
     
+    
     // transformations
     glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(pos.x + anchor_x, pos.y + anchor_y, 0.0f));
-    glm::mat4 scale_mat = glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, 0.0f));
-    glm::mat4 rot = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
-        
-    glm::mat4 mvp = p2d::Director::Inst()->getProjection() * rot * trans * scale_mat;
-    // TODO: rotation
+    glm::mat4 mvp = p2d::Director::Inst()->getProjection() * trans;
+    
+    if (angle != 0.0f)
+        mvp *= glm::rotate(trans, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+    
+    if (scale != 1.0f)
+        mvp *= glm::scale(glm::mat4(1.0f), glm::vec3(scale, scale, 0.0f));
+    
     
     p2d::ShaderManager::Inst()->useProgram("move_color");
     
@@ -55,41 +60,3 @@ void p2d::Actor::draw() {
     
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
-
-//void p2d::Actor::draw() {
-//    transformForScale();
-//    
-//    static const GLfloat squareVertices[] = {
-//        0.0f, 0.0f, 0.0f,
-//        scaled_dim_x, 0.0f, 0.0f,
-//        0.0f, scaled_dim_y, 0.0f,
-//        scaled_dim_x, scaled_dim_y, 0.0f
-//    };
-//    
-//    static const GLfloat textureVerticies[] = {
-//        0.0f, 0.0f, 0.0f,
-//        1.0f, 0.0f, 0.0f,
-//        0.0f, 1.0f, 0.0f,
-//        1.0f, 1.0f, 0.0f
-//    };
-//
-//    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-//    p2d::TextureManager::Inst()->bindTexture(image);
-//    
-//    // move and rotate the geometry
-//    transformForAnchor();
-//    glLoadIdentity();
-//    glTranslatef(pos.x, pos.y, 0.0f);
-//    
-//    // handle states and draw the geometry and texture
-//    glEnableClientState(GL_VERTEX_ARRAY);
-//    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-//    
-//    glVertexPointer(3, GL_FLOAT, 0, squareVertices);
-//    glTexCoordPointer(3, GL_FLOAT, 0, textureVerticies);
-//    
-//    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-//
-//    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-//    glDisableClientState(GL_VERTEX_ARRAY);
-// }
--- plasma2d-ios/test_scene.cc
+++ plasma2d-ios/test_scene.cc
@@ -7,11 +7,11 @@
 #include <iostream>
 
 TestScene::TestScene() {
-    p2d::Actor *actor = new p2d::Actor(p2d::pxy(5.0f, 5.0f), "img_test.bmp");
+    p2d::Actor *actor = new p2d::Actor(p2d::pxy(100.0f, 105.0f), "img_test.bmp");
     actor->setAngle(45.0f);
     addObject(actor);
     
-//    p2d::Actor *actor2 = new p2d::Actor(p2d::pxy(300.0f, 300.0f), "img_test.png");
-//    actor2->setScale(3.0f);
-//    addObject(actor2);
+    p2d::Actor *actor2 = new p2d::Actor(p2d::pxy(500.0f, 300.0f), "img_test.png");
+    actor2->setScale(3.0f);
+    addObject(actor2);
 }
