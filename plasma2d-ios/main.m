8034d2adf9e460056aab970049bdb587b33ca889
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2011-04-23 14:56:14 -0600

    Redirect console output to /tmp/log-out.txt because Xcode 4.0.1 doesn't flush correctly.

--- plasma2d-ios/main.m
+++ plasma2d-ios/main.m
@@ -8,10 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+#define REDIRECT_STDOUT_TO_FILE 1
+
 int main(int argc, char *argv[])
 {
+#if REDIRECT_STDOUT_TO_FILE //## redirect stdout to "log-out.txt"
+    freopen("/tmp/log-out.txt", "w", stdout);
+#endif  //  REDIRECT_STDOUT_TO_FILE
+    
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
+    
+#if REDIRECT_STDOUT_TO_FILE //## redirect stdout to "log-out.txt"
+    fflush(stdout);
+    fclose(stdout);
+#endif  //  REDIRECT_STDOUT_TO_FILE
+
     return retVal;
 }
