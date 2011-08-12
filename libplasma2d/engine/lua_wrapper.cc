719df574e848863fe9d281543bb1ceb42300755c
Rob Merrell <robmerrell@i.tv>
2011-08-11 18:39:01 -0600

    Added lua and tolua++

---
+++ libplasma2d/engine/lua_wrapper.cc
@@ -0,0 +1,9 @@
+//
+//  lua_wrapper.cc
+//  plasma2d
+//
+//  Created by Rob Merrell on 8/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ libplasma2d/engine/lua_wrapper.h
@@ -0,0 +1,11 @@
+/**
+ * Licensed under the MIT license. See LICENSE for details.
+ * Copyright (C) 2011 Rob Merrell
+ */
+
+#ifndef P2D_LUA_WRAPPER_H
+#define P2D_LUA_WRAPPER_H
+
+
+
+#endif
