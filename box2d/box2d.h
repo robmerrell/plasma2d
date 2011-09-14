bc72d6e40fdd89e02879d9b309c4ae194a9aa798
Rob Merrell <robmerrell@i.tv>
2011-09-13 23:04:49 -0600

    Linked in Box2d

---
+++ box2d/box2d.h
@@ -0,0 +1,13 @@
+//
+//  box2d.h
+//  box2d
+//
+//  Created by Rob Merrell on 9/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface box2d : NSObject
+
+@end
